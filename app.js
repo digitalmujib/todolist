7dad3011d9012f3eb4eb264c248efd12c6e98889
mujib shaikh <mujibshaikh3231@gmail.com>
2022-07-08 15:20:44 +0530

    Added gitignore. procfile and updated ports

--- app.js
+++ app.js
@@ -148,6 +148,6 @@ app.get("/about", function (req, res) {
     res.render("about");
 })
 
-app.listen(3000, function () {
-    console.log("Server has started on port 3000");
+app.listen(process.eventNames.PORT || 3000, function () {
+    console.log("Server has started on port");
 })
